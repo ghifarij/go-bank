1d3d7d90a90869c632a7954d53e329772fbcf149
Afga Ghifari Jamaludin <ghifarij.25@gmail.com>
2023-10-26 20:33:51 +0700

    [FEAT] register

--- api/api.go
+++ api/api.go
@@ -22,6 +22,12 @@ type ErrResponse struct {
 	Message string
 }
 
+type Register struct {
+	Username string
+	Email    string
+	Password string
+}
+
 func login(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	helpers.HandleErr(err)
@@ -40,9 +46,28 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func register(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	helpers.HandleErr(err)
+
+	var formattedBody Register
+	err = json.Unmarshal(body, &formattedBody)
+	helpers.HandleErr(err)
+	register := users.Register(formattedBody.Username, formattedBody.Email, formattedBody.Password)
+
+	if register["message"] == "all is fine" {
+		resp := register
+		json.NewEncoder(w).Encode(resp)
+	} else {
+		resp := ErrResponse{Message: "wrong username or password"}
+		json.NewEncoder(w).Encode(resp)
+	}
+}
+
 func StartApi() {
 	router := mux.NewRouter()
 	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/register", register).Methods("POST")
 	fmt.Println("App is working on Port :8888")
 	log.Fatal(http.ListenAndServe(":8888", router))
 }
